7c7d177ed00995da3859d63c946fdd3c2a567cf6
Zarish Syed <zarish.syed1@gmail.com>
2016-09-07 16:22:09 +1200

    Add project files and folders

---
+++ js/prac.js
@@ -0,0 +1,20 @@
+function load() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("data").innerHTML = "";
+            var dataParse = xhttp.responseText;
+            var obj = JSON.parse(dataParse);
+            document.getElementById("data").innerHTML = obj.rates[convertTo] + "<br />";
+            
+            console.log(obj);
+
+        }
+    };
+    var base = document.getElementById("baseRate").value;
+    var convertTo = document.getElementById("compareWith").value;
+    console.log(convertTo);
+    
+    xhttp.open("GET", "http://api.fixer.io/latest?base="+base);
+    xhttp.send();
+}
